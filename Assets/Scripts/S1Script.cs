bb308da859a1db238a49477a9b022be03c2d6e9a
satyadev <satyagames@gmail.com>
2017-04-26 11:18:31 +0530

    first commit

---
+++ Assets/Scripts/S1Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class S1Script : MonoBehaviour {
+	public string levelToLoad = "s2";
+
+	public SceneFader sceneFader;
+
+	public void GotoS2 ()
+	{
+		sceneFader.FadeTo(levelToLoad);
+	}
+
+}
---
+++ Assets/Scripts/S2Script.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class S2Script : MonoBehaviour {
+	public string levelToLoad = "s1";
+
+	public SceneFader sceneFader;
+
+	public void GotoS1 ()
+	{
+		sceneFader.FadeTo(levelToLoad);
+	}
+
+}
